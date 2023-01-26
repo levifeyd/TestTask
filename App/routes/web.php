d8e998a2fd2e6d35ab77af38a00b9c53a0f8938d
levifeyd <kuleshov-as@mail.ru>
2023-01-26 12:00:10 +0400

    add welcome blade and roustes

--- App/routes/web.php
+++ App/routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\FeedbackController;
+use \Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +15,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
+
     return view('welcome');
+});
+Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('feedbacks', FeedbackController::class);
 });
