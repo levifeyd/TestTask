978c78c0be18f6ef1092e4aa3b2d6b39891cc6ea
levifeyd <kuleshov-as@mail.ru>
2023-01-26 12:13:24 +0400

    create models user and feedback

---
+++ App/app/Models/Feedback.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Feedback extends Model
+{
+    protected $table='feedbacks';
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'detail',
+        'filename','user_id',
+    ];
+    public $sortable = [
+        'created_at',
+    ];
+}
